daa4147ca06f19481078151150d1f217e4148033
zoemcl <zoemmclennan@gmail.com>
2018-05-15 16:32:41 -0400

    Working #1: Able to upload files
    users are now able to uplaod mei and png files. Redirection to the editor is also functional but is currently hardcoded to load the same mei file.

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,24 +1,64 @@
 var express = require('express');
 var fs = require('fs');
+var multer = require('multer');
 
 var router = express.Router();
 
+//////////////////
+/// Index routes//
+//////////////////
+
+var uploadi = multer({
+    dest: __dirname + '../../../public/img' ,
+    limits: {filesize: 1000000, files:1}
+})
+
+var uploadm = multer({
+    dest: __dirname + '../../../public/mei' ,
+    limits: {filesize: 1000000, files:1}
+})
+
 router.route('/')
-    .get(function (req, res) {
-        res.sendFile(__base + 'views/index.html');
+    .get(function (req, res) {    
+        fs.readdir(__base + 'public/mei', function(err, files){
+            if (err) {
+                res.status(500).send(error);
+                return
+            }
+            console.log(files);
+            res.render('index', {'files': files});
+        });
+});
+
+router.route('/upload_mei')
+    .post(uploadm.single("file"), function(req, res){
     });
 
-router.route('/save/:filename')
-    .post(function (req, res) {
-        fs.writeFile(__base + 'public/mei/' + req.body.fileName, 
-            req.body.meiData,
-            function(err) {
-                if(err) {
-                    return console.log(err);
-                }
-            }
-        )
-        console.log("File saved to " + req.body.fileName);
+router.route('/upload_img')
+    .post(uploadi.single("image"), function(req, res){    
     });
+
+router.route('/edit/:filename')
+    .get(function (req, res) {
+        console.log(res);
+        res.render('editor');
+    });
+
+/////////////////
+/// NEON routes//
+/////////////////
+
+router.route('/save/:filename')
+.post(function (req, res) {
+    fs.writeFile(__base + 'public/mei/' + req.body.fileName, 
+        req.body.meiData,
+        function(err) {
+            if(err) {
+                return console.log(err);
+            }
+        }
+    )
+    console.log("File saved to " + req.body.fileName);
+});
 
 module.exports = router;
