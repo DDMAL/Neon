48aa1438c9eaa47642a77e57e0f24e3735ebd459
Juliette Regimbal <juliette.regimbal@gmail.com>
2018-05-18 12:33:03 -0400

    Add zooming and panning

--- public/src/Neon.js
+++ public/src/Neon.js
@@ -5,6 +5,8 @@ function Neon (params) {
     //////////////
     var vrvToolkit = new verovio.toolkit();
     var fileName = params.meifile;
+    var zoomController = new ZoomController(this);
+
     var vrvOptions = {
         noLayout: 1,
         noFooter: 1,
@@ -18,6 +20,9 @@ function Neon (params) {
         saveMEI();
     });
 
+    // Set keypress listener
+    d3.select("body").on("keydown", keydownListener);
+
     ////////////
     // Functions
     ////////////
@@ -29,25 +34,51 @@ function Neon (params) {
     function loadPage () {
         var svg = vrvToolkit.renderToSVG(1);
         $("#svg_output").html(svg);
+        d3.select("#svg_output").select("svg").attr("id", "svg_container");
     }
 
     function refreshPage () {
         var meiData = vrvToolkit.getMEI();
         loadData(meiData);
+        zoomController.restoreTransformation();
     }
 
     function saveMEI() {
-        $(window).keydown(function(event) {
-            if (event.keyCode == 83) {
-                var meiData = vrvToolkit.getMEI();
-                $.ajax({
-                    type: "POST",
-                    url: "/save/" + fileName,
-                    data: {"meiData": meiData,
-                            "fileName": fileName}
-                }) 
-            }
-        });
+        var meiData = vrvToolkit.getMEI();
+        $.ajax({
+            type: "POST",
+            url: "/save/" + fileName,
+            data: {"meiData": meiData,
+                    "fileName": fileName}
+        }) 
+    }
+
+    function keydownListener () {
+        var unit = 10;
+        switch (d3.event.key) {
+            case "s":
+                saveMEI();
+                break;
+            case "z":
+                zoomController.zoom(1.25);
+                break;
+            case "Z":
+                zoomController.zoom(0.80);
+                break;
+            case "k":
+                zoomController.translate(0, unit);
+                break;
+            case "j":
+                zoomController.translate(0, -unit);
+                break;
+            case "l":
+                zoomController.translate(-unit, 0);
+                break;
+            case "h":
+                zoomController.translate(unit, 0);
+                break;
+            default: break;
+        }
     }
     // Constructor reference
     Neon.prototype.constructor = Neon;
