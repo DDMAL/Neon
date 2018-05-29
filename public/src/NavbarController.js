d8faa37d33c2dabea149ce40ae569e7ba8bbff77
zoemcl <zoemmclennan@gmail.com>
2018-05-29 10:12:26 -0400

    #27: File download works.
    Users can now download edited mei files and the original background image files

--- public/src/NavbarController.js
+++ public/src/NavbarController.js
@@ -1,9 +1,18 @@
 function NavbarController(filename) { 
     // setup navbar listeners 
+    // revert listener
     $("#revert").on("click", function() {
         $.ajax({
             type: "POST",
             url: "/revert/" + filename,
         })
     });
+
+    //mei download link
+    $("#getmei").attr("href", '/../uploads/mei/' + filename);
+
+    //png download setup
+    var pngFile = filename.split('.', 2)[0] + ".png";
+    $("#getpng").attr("href", '/../uploads/png/' + pngFile);
+    
 }
