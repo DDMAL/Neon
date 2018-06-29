ec4c95da9a6fbea661bb3ee507695c4cbcddae5a
zoemcl <zoemmclennan@gmail.com>
2018-06-29 09:36:53 -0400

    Added true to selected

--- src/DragSelect.js
+++ src/DragSelect.js
@@ -48,7 +48,7 @@ export default function DragSelect () {
         elements.map(function(el){
             d3.select(el)
                 .attr("fill", "#d00")
-                .attr("selected");
+                .attr("selected", "true");
         })
 
         d3.selectAll("#selectRect").remove();
