fd96ca43d95c3d72b1faaca3ca06d1ecc04621c7
Juliette Regimbal <juliette.regimbal@gmail.com>
2018-08-08 09:54:18 -0400

    Create pages-only version of editor.js so we can merge into develop eventually

---
+++ pages/editor.js
@@ -0,0 +1,7 @@
+import NeonView from "./NeonView.js";
+
+var view = new NeonView({
+    meifile: meiFile,
+    bgimg: bgImg,
+    mode: "pages"
+});
--- src/editor.js
+++ src/editor.js
@@ -1,7 +1,26 @@
 import NeonView from "./NeonView.js";
 
+if (autosave) {
+    let url = window.location.href;
+    let regex = /\/edit\//;
+    if (window.confirm("Newer autosave detected! Do you want to recover lost work?")) {
+        $.ajax({
+            type: "POST",
+            url: url.replace(regex, "/restore/"),
+            data: {}
+        });
+    } else {
+        $.ajax({
+            type: "POST",
+            url: url.replace(regex, "/autosave-clear/"),
+            data: {}
+        });
+    }
+}
+
+
 var view = new NeonView({
     meifile: meiFile,
     bgimg: bgImg,
-    mode: "pages"
+    mode: "standalone"
 });
