b340c262c054c01637ef50373c2032ca5e051a90
Juliette Regimbal <juliette.regimbal@gmail.com>
2018-08-27 13:52:39 -0400

    Start writing a SplitHandler class.
    Will use SplitHandler to handle the DOM events when splitting, finding
    the staff to split and where on that staff to split, and sending that
    action to verovio. Is modeled off InsertHandler.

---
+++ src/SplitHandler.js
@@ -0,0 +1,74 @@
+import * as Cursor from "./Cursor.js";
+import * as Notification from "./Notification.js";
+
+/**
+ * Handler splitting a staff into two staves through Verovio.
+ * @constructor
+ * @param {NeonView} neonView - The NeonView parent.
+ */
+function SplitHandler (neonView) {
+    function startSplit() {
+        removeSplitHandlers();
+
+        $("body").on("click", "#svg_output", handler);
+
+        // Handle keypresses
+        $("body").on("keydown", keydownListener);
+        $("body").on("keyup", resetHandler);
+        $("body").on("click", clickawayHandler);
+
+        Notification.queueNotification("Click Where to Split");
+    }
+
+    function keydownListener (evt) {
+        if (evt.key === "Escape") {
+            splitDisable();
+        }
+        else if (evt.key === "Shift") {
+            $("body").off("click", "#svg_output", handler);
+        }
+    }
+
+    function clickawayHandler (evt) {
+        if (evt.target.id != "svg_group" && $("#svg_group").find(evt.target).length === 0 && evt.target.tagName !== "path"
+            && evt.target.id != "split-system") {
+            splitDisable();
+            $("body").off("click", "#svg_output", handler);
+        }
+    }
+
+    function resetHandler (evt) {
+        if (evt.key === "Shift") {
+            $("body").on("click", "#svg_output", handler);
+        }
+    }
+
+    function handler (evt) {
+        var container = document.getElementsByClassName("definition-scale")[0];
+        var pt = container.createSVGPoint();
+        pt.x = evt.clientX;
+        pt.y = evt.clientY;
+
+        //Transform to SVG coordinate system.
+        var transformMatrix = container.getScreenCTM().inverse();
+        var cursorPt = pt.matrixTransform(transformMatrix);
+
+        // Find staff point corresponds to if one exists
+        // TODO
+
+        let editorAction = {
+            "action": "split",
+            "param": {
+                "elementId": id,
+                "x": cursorPt.x
+            }
+        };
+
+        neonView.edit(editorAction);
+        neonView.refreshPage();
+    }
+
+    SplitHandler.prototype.constructor = SplitHandler;
+    SplitHandler.prototype.startSplit = startSplit;
+}
+export {SplitHandler as default};
