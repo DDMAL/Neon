718c81f3436258f6765029fa3b885f03b8097573
Juliette Regimbal <juliette.regimbal@gmail.com>
2019-04-25 14:26:59 -0400

    Disable loading animation at part of execution.
    If we support Safari we can't rely on the load event to be triggered for
    SVG elements. Even though this does seem to be the behavior defined by
    W3, it just doesn't work in Safari (and presumably other webkit
    browsers).
    
    Code for the animation is kept in case we can find a more permanent
    solution, but since we want to move to diva.js and won't be directly
    loading images this isn't a high priority.
    
    Resolve #320

--- src/NeonView.js
+++ src/NeonView.js
@@ -92,6 +92,7 @@ function NeonView (params) {
             $("#svg_group").attr("width", "100%")
                 .attr("height", viewHeight)
                 .attr("viewBox", "0 0 " + width + " " + height);
+            hideLoad();
         }
         else {
             loadSvg();
