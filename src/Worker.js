a7a9a270a62bb0adc53975c575e841f4fc727e87
Juliette Regimbal <juliette.regimbal@gmail.com>
2019-05-06 11:11:07 -0400

    Use cleaned up version of xml.js
    Using version from DDMAL/xml.js@ba230e7 (which is open in a PR to the
    upstream repository). Requires specifying that the schema is RELAX NG
    instead of assuming like in our previous version.

--- src/Worker.js
+++ src/Worker.js
@@ -3,7 +3,8 @@ const xmllint = require('./validation/xmllint.js');
 onmessage = (event) => {
   let results = xmllint.validateXML({
     xml: event.data.mei,
-    schema: event.data.schema
+    schema: event.data.schema,
+    format: 'rng'
   });
   postMessage(results.errors);
 };
