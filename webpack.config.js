c144157f6895e28fddb1aa8a1f8f9787792075a0
Juliette Regimbal <juliette.regimbal@gmail.com>
2019-05-07 10:25:29 -0400

    Update version of verovio, prompt when grouping will succeed.

--- webpack.config.js
+++ webpack.config.js
@@ -30,7 +30,7 @@ module.exports = {
             loader: 'file-loader',
             options: {
               name: '[name].[ext]',
-              outputPath: '/img/'
+              outputPath: './img/'
             }
           }
         ]
