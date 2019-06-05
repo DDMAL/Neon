3ca3d9c78e95fbcd6d8cfc4191f08cf44accc46c
hutnyk <jacob.hutnyk@gmail.com>
2019-06-05 15:38:43 -0400

    html contents files refactoring

---
+++ src/utils/EditContents.js
@@ -0,0 +1,17 @@
+/**
+ * Contents of navbar menu after switching to edit mode.
+ * @type {string}
+ */
+export const navbarDropdownMenu =
+    "<div class='navbar-item has-dropdown is-hoverable'><a class='navbar-link'>File</a>" +
+    "<div id='navbar-dropdown-options' class='navbar-dropdown'>" +
+    "<a id='save' class='navbar-item'>Save File</a>" +
+    "<a id='getmei' class='navbar-item' href='' download=''> Download MEI </a>" +
+    "<a id='revert' class='navbar-item'> Revert </a>";
+
+/**
+ * Finalize option in the navbar for rodan
+ * @type {string}
+ */
+export const navbarFinalize =
+    "<a id='finalize' class='navbar-item'> Finalize MEI </a>";
