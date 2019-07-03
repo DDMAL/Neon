e02c86aeee181a9987e6d3d6644ebbaf8ff0874f
Juliette Regimbal <juliette.regimbal@gmail.com>
2019-07-03 08:49:20 -0400

    Add glyph opacity test

--- test/DivaBrowser.test.js
+++ test/DivaBrowser.test.js
@@ -41,4 +41,30 @@ describe.each(['firefox', 'chrome', 'safari'])('Tests on %s', (title) => {
       expect(title).toBe('Neon');
     });
   });
+
+  describe('Check Display Panel', () => {
+    beforeAll(async () => {
+      // Ensure document loaded
+      await browser.wait(until.elementLocated(By.id('opacitySlider')), 2000);
+    });
+
+    test('Check Glyph Opacity', async () => {
+      // Set opacity to 0
+      let glyphOpacitySlider = await browser.findElement(By.id('opacitySlider'));
+      let rect = await glyphOpacitySlider.getRect();
+      await browser.actions().dragAndDrop(glyphOpacitySlider, { x: -1 * parseInt(rect.width / 2), y: 0 }).perform();
+      let opacityText = await browser.findElement(By.id('opacityOutput')).getText();
+      expect(opacityText).toBe('0');
+      let containerStyle = await browser.findElement(By.className('neon-container')).getAttribute('style');
+      expect(containerStyle).toContain('opacity: 0;');
+
+      // Reset opacity to 1
+      let opacityButton = await browser.findElement(By.id('reset-opacity'));
+      await browser.actions().click(opacityButton).perform();
+      opacityText = await browser.findElement(By.id('opacityOutput')).getText();
+      containerStyle = await browser.findElement(By.className('neon-container')).getAttribute('style');
+      expect(opacityText).toBe('100');
+      expect(containerStyle).toContain('opacity: 1;');
+    });
+  });
 });
