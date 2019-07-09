dd6bef954b9b5ca01021bdf587a2f7fcd25e172a
Juliette Regimbal <juliette.regimbal@gmail.com>
2019-07-09 14:40:18 -0400

    Fix things in ConvertMei

--- src/utils/ConvertMei.js
+++ src/utils/ConvertMei.js
@@ -8,35 +8,35 @@ export function convertStaffToSb (staffBasedMei) {
   let meiTree = et.parse(staffBasedMei);
   let meiTag = meiTree.getroot();
 
-  for (let section of meiTag.findall('.//{http://www.music-encoding.org/ns/mei}section')) {
-    let newStaff = et.Element('{http://www.music-encoding.org/ns/mei}staff', { 'n': '1' });
-    let container = et.SubElement(newStaff, '{http://www.music-encoding.org/ns/mei}layer');
+  for (let section of meiTag.findall('.//section')) {
+    let newStaff = et.Element('staff', { 'n': '1' });
+    let container = et.SubElement(newStaff, 'layer');
     container.set('n', '1');
 
     for (let staff of section.getchildren()) {
       for (let layer of staff.getchildren()) {
         // Replace every staff + layer with a sb with the same facsimile info
-        let sb = et.Element('{http://www.music-encoding.org/ns/mei}sb', {
+        let sb = et.Element('sb', {
           'n': staff.get('n'),
           'facs': staff.get('facs'),
-          '{http://www.w3.org/XML/1998/namespace}id': staff.get('{http://www.w3.org/XML/1998/namespace}id')
+          'xml:id': staff.get('xml:id')
         });
 
         // Check for custos
         if (container.len() > 1) {
-          if (container.getItem(container.len() - 1).tag === '{http://www.music-encoding.org/ns/mei}custos') {
+          if (container.getItem(container.len() - 1).tag === 'custos') {
             sb.append(container.getItem(container.len() - 1));
             container.remove(container.getItem(container.len() - 1));
           }
         }
 
         // Check if first syllable has @follows
-        let firstSyllable = layer.find('{http://www.music-encoding.org/ns/mei}syllable');
+        let firstSyllable = layer.find('syllable');
         let syllableId = '';
         if (firstSyllable) {
           if (firstSyllable.get('follows')) {
             syllableId = firstSyllable.get('follows');
-            for (let syl of firstSyllable.findall('{http://www.music-encoding.org/ns/mei}syl')) {
+            for (let syl of firstSyllable.findall('syl')) {
               firstSyllable.remove(syl);
             }
             layer.remove(firstSyllable);
@@ -49,17 +49,17 @@ export function convertStaffToSb (staffBasedMei) {
           container.append(sb);
         } else {
           let syllable = container.find(
-            './/*[@{http://www.w3.org/XML/1998/namespace}id=\'' + syllableId + '\']'
+            './/*[@xml:id=\'' + syllableId + '\']'
           );
           syllable.append(sb);
-          syllable.extend(firstSyllable.getchildren());
+          syllable._children = syllable._children.concat(firstSyllable.getchildren());
         }
-        container.extend(layer.getchildren());
+        container._children = container._children.concat(layer.getchildren());
       }
     }
-    let sectionId = section.get('{http://www.w3.org/XML/1998/namespace}id');
+    let sectionId = section.get('xml:id');
     section.clear();
-    section.set('{http://www.w3.org/XML/1998/namespace}id', sectionId);
+    section.set('xml:id', sectionId);
     section.append(newStaff);
   }
 
@@ -70,19 +70,19 @@ export function convertSbToStaff (sbBasedMei) {
   let meiTree = et.parse(sbBasedMei);
   let meiTag = meiTree.getroot();
 
-  for (let section of meiTag.findall('.//{http://www.music-encoding.org/ns/mei}section')) {
+  for (let section of meiTag.findall('.//section')) {
     let staffStore = [];
     for (let staff of section.getchildren()) {
       for (let layer of staff.getchildren()) {
         let sbIndexes = [];
-        for (let sb of layer.findall('{http://www.music-encoding.org/ns/mei}sb')) {
+        for (let sb of layer.findall('sb')) {
           sbIndexes.append(layer.getchildren().indexOf(sb));
         }
 
         for (let [sbIndex, n] of zip(sbIndexes, [...Array(sbIndexes.length).keys()])) {
           let sb = layer.getIndex(sbIndex);
-          let newStaff = et.Element('{http://www.music-encoding.org/ns/mei}staff', sb.attrib);
-          let container = et.SubElement(newStaff, '{http://www.music-encoding.org/ns/mei}layer');
+          let newStaff = et.Element('staff', sb.attrib);
+          let container = et.SubElement(newStaff, 'layer');
 
           // Check for custos
           for (let custos in sb.getchildren()) {
@@ -94,54 +94,54 @@ export function convertSbToStaff (sbBasedMei) {
 
           // Get elements to add
           let lastIndex = n + 1 === sbIndexes.length ? layer.len() : sbIndexes.getIndex(n + 1);
-          container.extend(layer.getSlice(sbIndex + 1, lastIndex));
+          container._children = container._children.concat(layer.getSlice(sbIndex + 1, lastIndex));
           staffStore.append(newStaff);
         }
       }
     }
-    let sectionId = section.get('{http://www.w3.org/XML/1998/namespace}id');
+    let sectionId = section.get('id');
     section.clear();
-    section.set('{http://www.w3.org/XML/1998/namespace}id', sectionId);
-    section.extend(staffStore);
+    section.set('xml:id', sectionId);
+    section._children = section._children.concat(staffStore);
 
     // Handle sb in syllables
     let stavesAdded = 0;
     for (let [staff, staffIndex] of zip(section.getchildren(), [...Array(section.len()).keys()])) {
       for (let layer of staff.getchildren()) {
-        for (let syllable of layer.findall('.//{http://www.music-encoding.org/ns/mei}syllable')) {
-          let sb = syllable.find('{http://www.music-encoding.org/ns/mei}sb');
+        for (let syllable of layer.findall('.//syllable')) {
+          let sb = syllable.find('sb');
           if (!sb) {
             continue;
           }
-          let newStaff = et.Element('{http://www.music-encoding.org/ns/mei}staff', sb.attrib);
-          let newLayer = et.SubElement(newStaff, '{http://www.music-encoding.or/ns/mei}layer');
+          let newStaff = et.Element('staff', sb.attrib);
+          let newLayer = et.SubElement(newStaff, 'layer');
           newLayer.set('n', '1');
 
           let newSyllableId = uuid();
-          let newSyllable = et.SubElement(newLayer, '{http://www.music-encoding.org/ns/mei}syllable');
-          newSyllable.set('follows', syllable.get('{http://www.w3.org/XML/1998/namespace}id'));
-          newSyllable.set('{http://www.w3.org/XML/1998/namespace}id', newSyllableId);
+          let newSyllable = et.SubElement(newLayer, 'syllable');
+          newSyllable.set('follows', syllable.get('xml:id'));
+          newSyllable.set('xml:id', newSyllableId);
           syllable.set('precedes', newSyllableId);
-          newSyllable.extend(syllable.getSlice(syllable.getchildren().indexOf(sb) + 1, syllable.len()));
+          newSyllable._children = newSyllable._children.concat(syllable.getSlice(syllable.getchildren().indexOf(sb) + 1, syllable.len()));
 
           let oldSyllableContent = syllable.getSlice(0, syllable.getchildren().indexOf(sb));
           let syllableAttrib = syllable.attrib;
 
           // Move remaining components to new staff.
-          newLayer.extend(layer.getSlice(layer.getchildren().indexOf(syllable) + 1, layer.len()));
+          newLayer._children = newLayer._children.concat(layer.getSlice(layer.getchildren().indexOf(syllable) + 1, layer.len()));
           let layerAttrib = layer.attrib;
           let layerContent = layer.getSlice(0, layer.getchildren().indexOf(syllable) + 1);
           layer.clear();
           layer.attrib = layerAttrib;
-          layer.extend(layerContent);
+          layer._children = layer._children.concat(layerContent);
 
           // Handle custos;
-          layer.extend(sb.getchildren());
+          layer._children = layer._children.concat(sb.getchildren());
 
           // Shrink syllable
           syllable.clear();
           syllable.attrib = syllableAttrib;
-          syllable.extend(oldSyllableContent);
+          syllable._children = syllable._children.concat(oldSyllableContent);
 
           section.insert(staffIndex + stavesAdded + 1, newStaff);
         }
