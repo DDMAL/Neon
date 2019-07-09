f15449eaa24fe5f6076c4273c48364bef28ee8ff
Juliette Regimbal <juliette.regimbal@gmail.com>
2019-07-09 14:45:32 -0400

    Add test for converting staff to sb

---
+++ test/ConvertMei.test.js
@@ -0,0 +1,18 @@
+/* eslint-env jest */
+
+import * as ConvertMei from '../src/utils/ConvertMei.js';
+
+const fs = require('fs');
+const et = require('elementtree');
+var originalTest, sbKey;
+
+beforeAll(() => {
+  originalTest = fs.readFileSync('./test/resources/test.mei').toString();
+  sbKey = fs.readFileSync('./test/resources/testSb.mei').toString();
+});
+
+test('Verify results of \'convertStaffToSb\'', () => {
+  let result = ConvertMei.convertStaffToSb(originalTest);
+  let parsedKey = et.parse(sbKey).write({ xml_declaration: true });
+  expect(result).toBe(parsedKey);
+});
