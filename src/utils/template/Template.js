136bb23bee75804f4ad6b178accbce01f1d80487
Juliette Regimbal <juliette.regimbal@gmail.com>
2019-07-17 15:02:12 -0400

    Set location of Neon home button in webpack config

--- src/utils/template/Template.js
+++ src/utils/template/Template.js
@@ -2,6 +2,7 @@ const template = require('./template.html');
 
 function setBody () {
   document.body.innerHTML = template;
+  document.getElementById('home-link').href = LINK_LOCATION;
 }
 
 export { setBody as default };
