0c85c4921fc8ea41f0e3c9c7e6ef9c79675ebd80
Juliette Regimbal <juliette.regimbal@gmail.com>
2019-08-15 08:04:19 -0400

    Lint javascript files and typescript

--- src/Validation.ts
+++ src/Validation.ts
@@ -1,26 +1,51 @@
-/** @module Validation */
-
 // const schemaPromise = import('./validation/mei-all.rng');
 const schemaPromise = require('./validation/mei-all.rng');
 const Worker: WorkerConstructable = require('./Worker.js');
-var worker: Worker, schema: { default: any }, statusField: HTMLSpanElement;
+let worker: Worker, schema: { default: string[] }, statusField: HTMLSpanElement;
 
 interface WorkerConstructable {
   new (): Worker;
 }
 
 /**
+ * Update the UI with the validation results. Called when the WebWorker finishes validating.
+ */
+function updateUI (message: { data: string[] }): void {
+  const errors = message.data;
+  if (errors === null) {
+    statusField.textContent = 'VALID';
+    statusField.style.color = 'green';
+    for (const child of statusField.children) {
+      child.remove();
+    }
+  } else {
+    let log = '';
+    errors.forEach(line => {
+      log += line + '\n';
+    });
+    statusField.textContent = '';
+    statusField.style.color = 'red';
+    const link = document.createElement('a');
+    link.setAttribute('href', 'data:text/plain;charset=utf-8,' +
+      encodeURIComponent(log));
+    link.setAttribute('download', 'validation.log');
+    link.textContent = 'INVALID';
+    statusField.appendChild(link);
+  }
+}
+
+/**
  * Add the validation information to the display and create the WebWorker
  * for validation MEI.
  */
-export async function init () {
-  let displayContents = document.getElementById('displayContents');
+export async function init (): Promise<void> {
+  const displayContents = document.getElementById('displayContents');
   if (displayContents !== null) {
-    let panelBlock = document.createElement('div');
+    const panelBlock = document.createElement('div');
     panelBlock.classList.add('panel-block');
-    let pNotif = document.createElement('p');
+    const pNotif = document.createElement('p');
     pNotif.textContent = 'MEI Status: ';
-    let span = document.createElement('span');
+    const span = document.createElement('span');
     span.id = 'validation_status';
     span.textContent = 'unknown';
     pNotif.appendChild(span);
@@ -36,7 +61,7 @@ export async function init () {
  * Send the contents of an MEI file to the WebWorker for validation.
  * @param {string} meiData
  */
-export async function sendForValidation (meiData: string) {
+export async function sendForValidation (meiData: string): Promise<void> {
   if (statusField === undefined) {
     return;
   }
@@ -52,39 +77,12 @@ export async function sendForValidation (meiData: string) {
 }
 
 /**
- * Update the UI with the validation results. Called when the WebWorker finishes validating.
- */
-function updateUI (message: { data: any }): void {
-  let errors = message.data;
-  if (errors === null) {
-    statusField.textContent = 'VALID';
-    statusField.style.color = 'green';
-    for (let child of statusField.children) {
-      child.remove();
-    }
-  } else {
-    let log = '';
-    errors.forEach(line => {
-      log += line + '\n';
-    });
-    statusField.textContent = '';
-    statusField.style.color = 'red';
-    let link = document.createElement('a');
-    link.setAttribute('href', 'data:text/plain;charset=utf-8,' +
-      encodeURIComponent(log));
-    link.setAttribute('download', 'validation.log');
-    link.textContent = 'INVALID';
-    statusField.appendChild(link);
-  }
-}
-
-/**
  * Update the message on a blank page when there is no MEI.
  */
-export function blankPage () {
-  for (let child of statusField.children) {
+export function blankPage (): void {
+  for (const child of statusField.children) {
     child.remove();
   }
-  statusField.textContent = 'No MEI'
+  statusField.textContent = 'No MEI';
   statusField.style.color = 'color:gray';
 }
