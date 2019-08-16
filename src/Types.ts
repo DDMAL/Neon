fd7880df3218b9f9d1003ff716855481f22d2b22
Juliette Regimbal <juliette.regimbal@gmail.com>
2019-08-16 11:08:32 -0400

    Comments explaining defined types

--- src/Types.ts
+++ src/Types.ts
@@ -2,11 +2,13 @@
 
 export type Attributes = { pname?: string; oct?: number; shape?: string; line?: number; ligated?: boolean };
 
+/** An editing action sent to verovio as described [here](https://github.com/DDMAL/Neon/wiki/Toolkit-Actions). */
 export type EditorAction = {
   action: string;
   param: object | EditorAction[];
 };
 
+/** A message sent to the verovio web worker. */
 export type VerovioMessage = {
   action: string;
   id: string;
