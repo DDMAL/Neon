b952e230b68d3eb03d6f1b391217107d7e358722
Juliette Regimbal <juliette.regimbal@gmail.com>
2019-08-19 13:31:59 -0400

    Convert Zoom test to TypeScript

--- test/Zoom.test.ts
+++ test/Zoom.test.ts
@@ -1,21 +1,21 @@
 /* eslint-env jest */
 
-const ViewBox = require('../src/SingleView/Zoom').ViewBox;
+import { ViewBox } from '../src/SingleView/Zoom';
 
 test('Check ViewBox constructor', () => {
-  let viewBox = new ViewBox(300, 400);
+  const viewBox = new ViewBox(300, 400);
   expect(viewBox.c).toBe(300);
   expect(viewBox.d).toBe(400);
 });
 
-test("Check 'get' function", () => {
-  let viewBox = new ViewBox();
+test('Check \'get\' function', () => {
+  const viewBox = new ViewBox(100, 100);
   viewBox.set(0, 0, 100, 100);
   expect(viewBox.get()).toBe('0 0 100 100');
 });
 
-test("Check 'translate' function", () => {
-  let viewBox = new ViewBox();
+test('Check \'translate\' function', () => {
+  const viewBox = new ViewBox(100, 100);
   viewBox.set(0, 0, 100, 100);
   viewBox.translate(25, 35);
   expect(viewBox.a).toBe(25);
@@ -24,8 +24,8 @@ test("Check 'translate' function", () => {
   expect(viewBox.d).toBe(100);
 });
 
-test("Check 'zoomTo' function", () => {
-  let viewBox = new ViewBox(100, 200);
+test('Check \'zoomTo\' function', () => {
+  const viewBox = new ViewBox(100, 200);
   viewBox.zoomTo(2);
   expect(viewBox.c).toBe(50);
   expect(viewBox.d).toBe(100);
