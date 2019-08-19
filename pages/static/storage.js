237f3d856c0af0d4ee0f891965253459abc7a79f
Juliette Regimbal <juliette.regimbal@gmail.com>
2019-08-19 10:07:39 -0400

    Use UUIDv4 strings as IDs

--- pages/static/storage.js
+++ pages/static/storage.js
@@ -1,5 +1,38 @@
 const db = new PouchDB('Neon-User-Storage');
 
+function uint8ToUuid(data) {
+  if (data.length !== 16) {
+    return '';
+  }
+  function cb(previous, current) {
+    return previous + current.toString(16).padStart(2, '0');
+  }
+  return data.slice(0, 4).reduce(cb, '') +
+        '-' + data.slice(4, 6).reduce(cb, '') +
+        '-' + data.slice(6, 8).reduce(cb, '') +
+        '-' + data.slice(8, 10).reduce(cb, '') +
+        '-' + data.slice(10).reduce(cb, '');
+}
+function uuidv4() {
+  // Check for crypto API
+  if (window.crypto === undefined) {
+    return uint8ToUuid(new Uint8Array(16));
+  }
+  // Get 16 octets for UUID
+  const octets = new Uint8Array(16);
+  const modifiers = Uint8Array.from([
+    parseInt('01000000', 2),
+    parseInt('10000000', 2),
+    parseInt('00001111', 2),
+    parseInt('00111111', 2) // Mask to zero higher bits, variant
+  ]);
+  window.crypto.getRandomValues(octets);
+  // Set version bits and variant bits
+  octets[6] = (octets[6] & modifiers[2]) | modifiers[0];
+  octets[8] = (octets[8] & modifiers[3]) | modifiers[1];
+  return uint8ToUuid(octets);
+}
+
 getAllDocuments().then(response => {
   const storage = document.getElementById('storage-selector');
   for (const doc of response.rows) {
@@ -82,7 +115,7 @@ function createManifest (mei, bg) {
         }
       }
     ],
-    '@id': 'test-id',
+    '@id': uuidv4(),
     'title': mei.name,
     'timestamp': (new Date()).toISOString()
   };
@@ -110,7 +143,7 @@ function createManifest (mei, bg) {
       manifest['image'] = bgUri;
       manifest['mei_annotations'] = [
         {
-          'id': 'test-id-2',
+          'id': uuidv4(),
           'type': 'Annotation',
           'body': meiUri,
           'target': bgUri
