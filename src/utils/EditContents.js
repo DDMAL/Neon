ef5dfa5862882650dbb87759f5862d38e415a962
Juliette Regimbal <juliette.regimbal@gmail.com>
2019-08-22 09:22:28 -0400

    Set up File dropdown using HTMLElements not raw HTML
    Basically commit 64cc25b but in JavaScript instead of TS.

--- src/utils/EditContents.js
+++ src/utils/EditContents.js
@@ -2,15 +2,26 @@
 
 /**
  * Contents of navbar menu after switching to edit mode.
- * @type {string}
  */
-export const navbarDropdownMenu =
-    "<div class='navbar-item has-dropdown is-hoverable'><a class='navbar-link'>File</a>" +
-    "<div id='navbar-dropdown-options' class='navbar-dropdown'>" +
-    "<a id='save' class='navbar-item'>Save</a>" +
-    "<a id='export' class='navbar-item'>Save and Export to File</a>" +
-    "<a id='getmei' class='navbar-item' href='' download=''> Download MEI </a>" +
-    "<a id='revert' class='navbar-item'> Revert </a>";
+export const navbarDropdownMenu = document.createElement('div');
+navbarDropdownMenu.classList.add('navbar-item', 'has-dropdown', 'is-hoverable');
+const fileLink = document.createElement('a');
+fileLink.classList.add('navbar-link');
+fileLink.textContent = 'File';
+const navbarContents = document.createElement('div');
+navbarContents.classList.add('navbar-dropdown');
+navbarContents.id = 'navbar-dropdown-options';
+const contents = [['save', 'Save'], ['export', 'Save and Export to File'],
+  ['getmei', 'Download MEI'], ['revert', 'Revert']];
+contents.forEach(content => {
+  const item = document.createElement('a');
+  item.id = content[0];
+  item.classList.add('navbar-item');
+  item.textContent.add('navbar-item');
+  navbarContents.appendChild(item);
+});
+navbarDropdownMenu.appendChild(fileLink);
+navbarDropdownMenu.appendChild(navbarContents);
 
 /**
  * Finalize option in the navbar for rodan
