e45449bbb69c377caf4ef2c3321d9b5ae39554d3
Juliette Regimbal <juliette.regimbal@gmail.com>
2020-03-04 14:34:59 -0500

    Re-add commit hashes to builds
    This should have been done after the last release.

--- webpack.pages-config.js
+++ webpack.pages-config.js
@@ -49,7 +49,7 @@ module.exports = {
     new HardSourceWebpackPlugin(),
     new webpack.DefinePlugin({
       __LINK_LOCATION__: JSON.stringify('https://ddmal.music.mcgill.ca/Neon'),
-      __NEON_VERSION__: JSON.stringify('v4.1.0'),
+      __NEON_VERSION__: JSON.stringify(commitHash),
       __ASSET_PREFIX__: JSON.stringify('/Neon/Neon-gh/')
     })
   ]
