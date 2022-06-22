0a3b5ecf474fb0d1e0782e93b6892082add13f59
Max Zhang <48145854+chetbae@users.noreply.github.com>
2022-06-22 10:12:24 -0400

    💄 loading indicator on upload

--- deployment/server/landing.ts
+++ deployment/server/landing.ts
@@ -11,12 +11,18 @@ document.getElementById('initial_upload_container').appendChild(InitialUploadAre
 // add function to pairing button
 document.getElementById('make_pair').addEventListener('click', handleMakePair);
 document.getElementById('upload_button').addEventListener('click', async () => {
+  const circle = document.getElementById('loading_circle');
+  circle.className = 'loading';
   handleUploadAllDocuments()
-    .then( () => {
-      updateAndClear();
+    .then( (res) => {
+      console.log('Upload results: ', res);
     })
     .catch( () => {
+      console.log('One or more uploads rejected');
+    })
+    .finally( () => {
       updateAndClear();
+      circle.className = '';
     });
 });
 
