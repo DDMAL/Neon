f3f935b7861190d11c26ba72e26e6b8860e6af7a
Jinho Yoon <yoonjinho0318@gmail.com>
2022-07-08 15:35:41 -0400

    ♻️ Move out code into functions and replace d3
    - d3.select() for .attr() is replaced with querySelector.
    - DragHandler.moveElements() and .returnElements() are created for repeated usage in dragging() and dragEnd()
    - createDragActions() is created as a no-side-effect function to create EditorActions. No mutating an array.

--- src/utils/DragHandler.ts
+++ src/utils/DragHandler.ts
@@ -7,7 +7,7 @@ import { queueNotification } from './Notification';
 class DragHandler {
   readonly neonView: NeonView;
   private selector: string;
-  private selection: Element[];
+  private selection: SVGGraphicsElement[];
 
   private dragStartCoords: [number, number] = [-1, -1];
   private resetToAction: (selection: d3.Selection<d3.BaseType, unknown, HTMLElement, unknown>, args: unknown[]) => void;
@@ -47,18 +47,16 @@ class DragHandler {
     const activeNc = d3.selectAll('.selected');
     activeNc.call(dragBehaviour);
 
-    const selection = Array.from(document.getElementsByClassName('selected'));
-    this.selection = selection.concat(Array.from(document.getElementsByClassName('resizePoint')));
+    const selection = Array.from(document.querySelectorAll<SVGGraphicsElement>('.selected'));
+    this.selection = selection.concat(Array.from(document.querySelectorAll<SVGGraphicsElement>('.resizePoint')));
   }
 
   dragging (): void {
     this.dx = d3.event.x - this.dragStartCoords[0];
     this.dy = d3.event.y - this.dragStartCoords[1];
-    this.selection.forEach((el) => {
-      d3.select(el).attr('transform', () => {
-        return 'translate(' + [this.dx, this.dy] + ')';
-      });
-    });
+
+    this.moveElements(this.selection, this.dx, this.dy);
+
     /*
      * if we're dragging a syllable (or neume etc) then there won't be a syl selected
      * then we don't want the bounding box (if it is displayed) to move when dragging the neumes
@@ -67,79 +65,42 @@ class DragHandler {
      */
     const syls = this.selection.filter((el) => el.classList.contains('syl'));
     if (syls.length === 0) {
-      d3.selectAll('.syllable.selected')
-        .selectAll('.sylTextRect-display')
-        .attr(
-          'transform',
-          () => 'translate(' + [-1 * this.dx, -1 * this.dy] + ')'
-        );
+      const bboxes = Array.from(document.querySelectorAll('.syllable.selected > .sylTextRect-display'));
+      this.returnElements(bboxes);
     }
   }
 
 
   dragEnded (): void {
-    const paramArray: EditorAction[] = [];
-    this.selection
-      .filter((el) => !el.classList.contains('resizePoint'))
-      .forEach((el) => {
-        const id = el.tagName === 'rect' ? el.closest('.syl').id : el.id;
-
-        // If the cursor is out of bounds, nothing should be inserted.
-        const endX = this.dragStartCoords[0] + this.dx;
-        const endY = this.dragStartCoords[1] + this.dy;
-        if (isOutOfSVGBounds(endX, endY))
-          return queueNotification('[FAIL] Glyph was placed out of bounds! Drag action failed.');
-
-        const dragAction: DragAction = {
-          action: 'drag',
-          param: {
-            elementId: id,
-            x: this.dx,
-            y: -this.dy,
-          }
-        };
-
-        paramArray.push(dragAction);
-
-        if (el.classList.contains('divLine') || el.classList.contains('accid') || el.classList.contains('custos')) {
-          const { clientX, clientY } = d3.event.sourceEvent;
-          const newStaff = getStaffIdByCoords(clientX, clientY);
-
-          const staffAction: ChangeStaffToAction = {
-            action: 'changeStaffTo',
-            param: {
-              elementId: id,
-              // if divline is moved to the background (and not a staff),
-              // set the staffId to the original staff
-              staffId: newStaff ? newStaff : el.closest('.staff').id,
-            }
-          };
-
-          paramArray.push(staffAction);
-        }
-      });
-
-    const editorAction: EditorAction = {
-      action: 'chain',
-      param: paramArray
-    };
-
-    const xDiff = Math.abs(this.dx);
-    const yDiff = Math.abs(this.dy);
-
-    if (xDiff > 5 || yDiff > 5) {
-      this.neonView
-        .edit(editorAction, this.neonView.view.getCurrentPageURI())
-        .then(() => {
-          this.neonView.updateForCurrentPage();
-          this.endOptionsSelection();
-          this.reset();
-          this.dragInit();
-        });
-    } else {
+    // If the selection has not been moved more than 5 pixels in any direction,
+    // do not do any action
+    const xDiff = Math.abs(this.dx), yDiff = Math.abs(this.dy);
+    if (xDiff <= 5 && yDiff <= 5) {
       this.reset();
       this.dragInit();
+      return;
     }
+
+    // Filter selection for draggable elements
+    const selection = this.selection.filter((el) => !el.classList.contains('resizePoint'));
+
+
+    // Create the chain editor action for selection
+    const dragActions = this.createDragActions(selection);
+    const editorAction: EditorAction = {
+      action: 'chain',
+      param: dragActions
+    };
+
+    // Send editor action
+    this.neonView
+      .edit(editorAction, this.neonView.view.getCurrentPageURI())
+      .then(() => {
+        this.neonView.updateForCurrentPage();
+        this.endOptionsSelection();
+        this.reset();
+        this.dragInit();
+      });
   }
 
   /** Set the d3 action to use for [[reset]]. */
@@ -160,6 +121,64 @@ class DragHandler {
       moreEdit.innerHTML = '';
       moreEdit.parentElement.classList.add('hidden');
     }
+  }
+
+  /**
+   * Visually move the selected elements by `dx` and `dy`; (not using d3)
+   */
+  moveElements (selection: Element[], dx: number, dy: number): void {
+    selection.forEach((el) => {
+      el.setAttribute('transform', `translate(${dx},${dy})`);
+    });
+  }
+
+  /**
+   * Visually return selected elements to their original positions
+   * by removing the transform attribute
+   */
+  returnElements (selection: Element[]): void {
+    selection.forEach((el) => {
+      el.removeAttribute('transform');
+    });
+  }
+
+  /**
+   * Create drag / change staff actions for the selection array
+   * @param {SVGGraphicsElement[]} selection Selected elements
+   * @returns {EditorAction[]} Drag / change staff actions
+   */
+  createDragActions (selection: SVGGraphicsElement[]): EditorAction[] {
+    return selection.reduce((arr, el) => {
+      const id = el.tagName === 'rect' ? el.closest('.syl').id : el.id;
+
+      const dragAction: DragAction = {
+        action: 'drag',
+        param: {
+          elementId: id,
+          x: this.dx,
+          y: -this.dy,
+        }
+      };
+
+      // If not a divline, accid, or custo, add only the DragAction
+      if (!(el.classList.contains('divLine') || el.classList.contains('accid') || el.classList.contains('custos')))
+        return arr.concat(dragAction);
+
+      // Else, also add the ChangeStaffAction (for divline, accid, or custo)
+      const { clientX, clientY } = d3.event.sourceEvent;
+      const newStaff = getStaffIdByCoords(clientX, clientY);
+      const staffAction: ChangeStaffToAction = {
+        action: 'changeStaffTo',
+        param: {
+          elementId: id,
+          // if divline is moved to the background (and not a staff),
+          // set the staffId to the original staff
+          staffId: newStaff || el.closest('.staff').id,
+        }
+      };
+
+      return arr.concat([dragAction, staffAction]);
+    }, []);
   }
 }
 
