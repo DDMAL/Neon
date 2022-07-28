34b9ab0b85d3df3327de57509cfa47fd3890ba5b
David Peterman <peterman.dd@gmail.com>
2022-07-28 14:17:45 -0400

    💡🍱 Update asset paths and update comments in Unsaved.ts

--- src/utils/Unsaved.ts
+++ src/utils/Unsaved.ts
@@ -1,16 +1,33 @@
-let saved = true;
 
+// when page first loads, show default icon (saved)
+let saved = true;
+const indicator = document.querySelector<HTMLDivElement>('#file-saved');
+indicator.setAttribute('src', `${__ASSET_PREFIX__}assets/img/saved-icon.svg`);
+
+
+/**
+ * Update status of saved status indicator
+ */
 function updateIndicator (): void {
-  const indicator = document.querySelector<HTMLDivElement>('#file-saved');
-  const path = saved? '/Neon/assets/img/saved-icon.svg' : '/Neon/assets/img/unsaved-icon.svg';
+  const path = saved? `${__ASSET_PREFIX__}assets/img/saved-icon.svg` : `${__ASSET_PREFIX__}assets/img/unsaved-icon.svg`;
   indicator.setAttribute('src', path);
 }
 
-export function setSavedStatus (status = false): void {
+
+/**
+ * Set the saved status of the folio
+ * 
+ * @param status boolean value. true if status is "saved", false if "not saved"
+ */
+export function setSavedStatus (status: boolean = false): void {
   saved = status;
   updateIndicator();
 }
 
+
+/**
+ * Function that defines event listener that checks for unsaved changes on page reload
+ */
 export function listenUnsavedChanges (): void {
   window.onbeforeunload = (e: BeforeUnloadEvent) => {
     if (!saved) {
