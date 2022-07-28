956ca43e237355e786767833d4e880a14888092e
David Peterman <peterman.dd@gmail.com>
2022-07-28 14:07:09 -0400

    💄 Add "Saved" and "Not Saved" Icons to Navbar
    - Added svg files for icons
    - Added proper css for new icons
    - Added typescript code to ensure appropriate icons are displayed

--- src/utils/Unsaved.ts
+++ src/utils/Unsaved.ts
@@ -2,7 +2,8 @@ let saved = true;
 
 function updateIndicator (): void {
   const indicator = document.querySelector<HTMLDivElement>('#file-saved');
-  indicator.style.display = saved ? 'none' : 'block';
+  const path = saved? '/Neon/assets/img/saved-icon.svg' : '/Neon/assets/img/unsaved-icon.svg';
+  indicator.setAttribute('src', path);
 }
 
 export function setSavedStatus (status = false): void {
