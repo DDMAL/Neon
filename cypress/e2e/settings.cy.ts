bd663d0405b462c397de0ab46722f51a0bd4d1d8
Jinho Yoon <yoonjinho0318@gmail.com>
2022-08-02 16:30:17 -0400

    🐛 Revert import in `settings.cy.ts`

--- cypress/e2e/settings.cy.ts
+++ cypress/e2e/settings.cy.ts
@@ -1,5 +1,3 @@
-import { Settings } from '../../src/utils/LocalSettings';
-
 beforeEach(() => {
   cy.visit('http://localhost:8080/editor.html?manifest=test');
   cy.get('#mei_output', { timeout: 10000 }).should('be.visible');
@@ -12,14 +10,14 @@ describe('test(settings): highlight', () => {
     // Check cmd/ctrl + r: should refresh the page without a change in highlight option
     cy.get('body').type('{meta}r');
     cy.window().then(win => {
-      const settings: Settings = JSON.parse(win.localStorage.getItem(LOCALSTORAGE_KEY));
+      const settings = JSON.parse(win.localStorage.getItem(LOCALSTORAGE_KEY));
       expect(settings.highlightMode).to.not.eq('layerElement');
     });
 
     // Check cmd/ctrl + q:
     cy.get('body').type('{meta}q');
     cy.window().then(win => {
-      const settings: Settings = JSON.parse(win.localStorage.getItem(LOCALSTORAGE_KEY));
+      const settings = JSON.parse(win.localStorage.getItem(LOCALSTORAGE_KEY));
       expect(settings.highlightMode).to.not.eq('staff');
     });
   });
