cb382ce02170f3601af95f22f6640b1876a73345
Jinho Yoon <yoonjinho0318@gmail.com>
2022-08-03 09:18:18 -0400

    ✅ Add tests for expected highlight localStorage behavior
    Pressing q/w/e/r/t/y should change highlight options correctly

--- cypress/e2e/settings.cy.ts
+++ cypress/e2e/settings.cy.ts
@@ -6,6 +6,44 @@ beforeEach(() => {
 const LOCALSTORAGE_KEY = 'samples/manifests/test.jsonld';
 
 describe('test(settings): highlight', () => {
+  it('key: highlight option should change on q/w/e/r/t/y', () => {
+    cy.get('body').type('q');
+    cy.window().then(win => {
+      const settings = JSON.parse(win.localStorage.getItem(LOCALSTORAGE_KEY));
+      expect(settings.highlightMode).to.eq('staff');
+    });
+
+    cy.get('body').type('w');
+    cy.window().then(win => {
+      const settings = JSON.parse(win.localStorage.getItem(LOCALSTORAGE_KEY));
+      expect(settings.highlightMode).to.eq('syllable');
+    });
+
+    cy.get('body').type('e');
+    cy.window().then(win => {
+      const settings = JSON.parse(win.localStorage.getItem(LOCALSTORAGE_KEY));
+      expect(settings.highlightMode).to.eq('neume');
+    });
+
+    cy.get('body').type('r');
+    cy.window().then(win => {
+      const settings = JSON.parse(win.localStorage.getItem(LOCALSTORAGE_KEY));
+      expect(settings.highlightMode).to.eq('layerElement');
+    });
+
+    cy.get('body').type('t');
+    cy.window().then(win => {
+      const settings = JSON.parse(win.localStorage.getItem(LOCALSTORAGE_KEY));
+      expect(settings.highlightMode).to.eq('selection');
+    });
+
+    cy.get('body').type('y');
+    cy.window().then(win => {
+      const settings = JSON.parse(win.localStorage.getItem(LOCALSTORAGE_KEY));
+      expect(settings.highlightMode).to.eq('none');
+    });
+  });
+
   it('key: no change if paired with cmd/ctrl', () => {
     // Check cmd/ctrl + r: should refresh the page without a change in highlight option
     cy.get('body').type('{meta}r');
