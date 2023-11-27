e29fb4584866db919f62b0ad42d012971d56dcad
Yinan Zhou <yinan.a.zhou@gmail.com>
2023-11-27 13:02:31 -0500

    Refactor filename conflict handler for upload new files

--- src/Dashboard/UploadTools.ts
+++ src/Dashboard/UploadTools.ts
@@ -236,6 +236,7 @@ export async function sortFileByName(sortByNameBtn: Element): Promise<void>  {
 //     });
 // }
 
+// Limit filename length
 export function formatFilename(filename: string, maxLen: number): string {
   const chunkLen = Math.floor(maxLen/2);
   const len = filename.length;
@@ -244,39 +245,13 @@ export function formatFilename(filename: string, maxLen: number): string {
 }
 
 // Renames file if there are naming conflicts, in the form of 'foobar (1)'
-export function fnConflictHandler(filename: string, comparisons: string[]): string {
-  const reg = new RegExp(filename);
-  const results = comparisons.filter((comparison: string) => reg.test(comparison));
+export function fnConflictHandler(filename: string, existingNames: string[]): string {
+  let newFilename = filename;
+  let counter = 1;
 
-  if (results.length !== 0) {
-    // Find lowest digit to name
-    const digitsReg = /\(\d+\),/g;
-    const soup = results.join().concat(',');
-    const digitsResults = soup.match(digitsReg);
-    
-    // If no digit matches then go to else statement
-    if (digitsResults !== null) {
-      const digits: number[] = digitsResults.map(str => {
-        const stripped = str.substring(1, str.length-2);
-        return Number(stripped);
-      });
-      digits.sort();
-
-      const idx = digits.indexOf(1);
-      if (idx === -1) return `${filename}(1)`;
-      else {
-        let prev = 1;
-        for (let i = idx+1 ; i < digits.length ; i++) {
-          const cur = digits[i];
-          if (cur !== prev + 1) {
-            break;
-          }
-          prev += 1;
-        }
-        return `${filename}(${prev+1})`;
-      }
-    }
-    else return `${filename}(1)`;
-  } 
-  else return filename;
+  while (existingNames.includes(newFilename)) {
+    newFilename = `${filename}(${counter})`;
+    counter++;
+  }
+  return newFilename;
 }
